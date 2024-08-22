f7ff502a6407d20c5025c0ebda8d7f4f97aa5b60
Marco Ribeiro <marco@basicondo.com.br>
2024-08-22 11:30:48 -0300

    Fixed typo in Substitutions property

--- FluentEmail.MailerSend/Models/Variable.cs
+++ FluentEmail.MailerSend/Models/Variable.cs
@@ -3,5 +3,5 @@
 public class Variable
 {
     public string Email { get; set; } = default!;
-    public IList<Substitution> Substituitions { get; set; } = new List<Substitution>();
+    public IList<Substitution> Substitutions { get; set; } = new List<Substitution>();
 }
